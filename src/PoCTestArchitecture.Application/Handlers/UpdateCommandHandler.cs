a33a72232b136f675a45534b9babe3a2b221f4b4
Carlos Eduardo Guimaraes Souza <carlos.guimaraes@iterative.com.br>
2024-10-24 10:17:36 -0300

    test: add handler test

---
+++ src/PoCTestArchitecture.Application/Handlers/UpdateCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using PoCTestArchitecture.Application.Handlers.Request;
+using PoCTestArchitecture.Application.Handlers.Response;
+
+namespace PoCTestArchitecture.Application.Handlers
+{
+    public class UpdateCommandHandler : ICommandHandler<CreateUserRequest, CreateUserResponse>
+    {
+        public Task<CreateUserResponse> Execute(CreateUserRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- test/PoCTestArchitecture.Architecture.Tests/Application/HandlerTests.cs
+++ test/PoCTestArchitecture.Architecture.Tests/Application/HandlerTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+using PoCTestArchitecture.Application.Handlers;
 
 namespace PoCTestArchitecture.Architecture.Tests.Application;
 public class HandlerTests : BaseTest
@@ -10,6 +8,14 @@ public class HandlerTests : BaseTest
     [Fact]
     public void CommandHandler_Should_HaveImplementInterface()
     {
+        var result = Types
+           .InAssembly(ApplicationAssembly)
+           .That()
+           .ImplementInterface(typeof(ICommandHandler<,>))
+           .Should()
+           .HaveNameEndingWith("CommandHandler")
+           .GetResult();
 
+        result.IsSuccessful.Should().BeTrue();
     }
 }
